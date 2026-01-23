1207c99c180dbf13bcb0492d183b838a758bd123
LukasSkywalker <git@lukasdiener.ch>
2026-01-23 10:16:02 +0100

    Fix path

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         has: [
           {
             type: "host",
-            value: "landing.bajour.ch",
+            value: "briefing.bajour.ch",
           },
         ],
         destination: "/bajour/:path*",
