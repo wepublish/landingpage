5d9db78b3dd85937adf5bca929e9899638324e67
LukasSkywalker <git@lukasdiener.ch>
2026-04-16 07:37:16 +0000

    Revert config.ts

--- app/ganzgraz/config.ts
+++ app/ganzgraz/config.ts
@@ -3,6 +3,6 @@ import { resolveListId } from "@/lib/resolve-interests";
 const TENANT = "GANZGRAZ";
 
 export async function resolveConfig() {
-  const listId = await resolveListId(TENANT, "Bajour"); // TODO: auf "ganzgraz" wechseln sobald Liste in Mailchimp angelegt
+  const listId = await resolveListId(TENANT, "ganzgraz");
   return { tenant: TENANT, listId };
 }
