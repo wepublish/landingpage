d82623727b85f73e58a597303e1e2c628f7fb42c
LukasSkywalker <git@lukasdiener.ch>
2026-04-16 10:16:20 +0000

    Add routing middleware

--- next.config.ts
+++ next.config.ts
@@ -4,30 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "briefing.bajour.ch",
-          },
-        ],
-        destination: "/bajour/:path*",
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "news.ganzgraz.at",
-          },
-        ],
-        destination: "/ganzgraz/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
