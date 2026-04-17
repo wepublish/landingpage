01cf61dfa61388f2b6fd3bffc93c56870d9e5fbb
LukasSkywalker <git@lukasdiener.ch>
2026-04-17 14:43:33 +0000

    Migrate middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const host = request.headers.get("host") ?? "";
   const { pathname } = request.nextUrl;
 
